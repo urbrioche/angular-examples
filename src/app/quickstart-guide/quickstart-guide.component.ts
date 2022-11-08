131dcdf1a9050cb72c85ce274dbe76de5b4fa04c
jerry <aafulin@gmail.com>
2022-11-08 19:55:47 +0800

    [recap] basic router

---
+++ src/app/quickstart-guide/quickstart-guide.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './quickstart-guide.component.html',
+  styleUrls: ['./quickstart-guide.component.css']
+})
+export class QuickstartGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
