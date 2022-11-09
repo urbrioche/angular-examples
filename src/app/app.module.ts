0ac86b79fda2ca54b11409d4225acce48d240615
jerry <aafulin@gmail.com>
2022-11-09 20:34:08 +0800

    [lab] quick start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuickstartGuideComponent } from './quickstart-guide/quickstart-guide.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {AgGridModule} from 'ag-grid-angular';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quickstart-guide/quickstart-guide.component.ts
+++ src/app/quickstart-guide/quickstart-guide.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {CellClickedEvent, ColDef} from 'ag-grid-community';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AgGridAngular} from 'ag-grid-angular';
 
 @Component({
   templateUrl: './quickstart-guide.component.html',
-  styleUrls: ['./quickstart-guide.component.css']
+  styleUrls: ['./quickstart-guide.component.scss']
 })
 export class QuickstartGuideComponent implements OnInit {
+  public colDefs: ColDef[] = [
+    { field: 'make'},
+    { field: 'model'},
+    { field: 'price' }
+  ];
 
-  constructor() { }
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  public rowData$!: Observable<Car[]>;
+
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.rowData$ = this.http
+      .get<Car[]>('assets/demo-data/row-data.json');
   }
 
+  onCellClicked(event: CellClickedEvent) {
+    console.log(event);
+  }
+
+  clearSelection() {
+    this.agGrid.api.deselectAll();
+  }
+}
+
+interface Car {
+  make: string;
+  model: string;
+  price: number;
 }
