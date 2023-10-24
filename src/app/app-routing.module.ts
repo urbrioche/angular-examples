8099716730b5b8bbfd2d8d51a90c0bb2b692af71
jerry <aafulin@gmail.com>
2023-10-24 20:48:04 +0800

    [chore] remove useHash router since liff endpoint not allow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,25 +8,27 @@ import {CustomComponentOverviewComponent} from './custom-component-overview/cust
 import {HighchartInGridOverviewComponent} from './highchart-in-grid-overview/highchart-in-grid-overview.component';
 import {PlotlyContourComponent} from './plotly-contour/plotly-contour.component';
 import {NgZoneOverviewComponent} from './ng-zone-overview/ng-zone-overview.component';
+import {LiffOverviewComponent} from './liff-overview/liff-overview.component';
 
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'quickstart-guide', component: QuickstartGuideComponent},
-  {path: 'enterprise-overview', component: EnterpriseOverviewComponent},
-  {path: 'cell-render-overview', component: CellRenderOverviewComponent},
-  {path: 'custom-component-overview', component: CustomComponentOverviewComponent},
-  {path: 'highchart-in-grid-overview', component: HighchartInGridOverviewComponent},
-  {path: 'plotly-contour', component: PlotlyContourComponent},
-  {path: 'ng-zone-overview', component: NgZoneOverviewComponent},
+    {path: '', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'quickstart-guide', component: QuickstartGuideComponent},
+    {path: 'enterprise-overview', component: EnterpriseOverviewComponent},
+    {path: 'cell-render-overview', component: CellRenderOverviewComponent},
+    {path: 'custom-component-overview', component: CustomComponentOverviewComponent},
+    {path: 'highchart-in-grid-overview', component: HighchartInGridOverviewComponent},
+    {path: 'plotly-contour', component: PlotlyContourComponent},
+    {path: 'ng-zone-overview', component: NgZoneOverviewComponent},
+    {path: 'liff-overview', component: LiffOverviewComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true,
-  })],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        // useHash: true
+    })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
